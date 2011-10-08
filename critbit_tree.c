0b8ebd049b83fe267f985eafe0556930a9c6f260
Ricardo Catalinas Jiménez <jimenezrick@gmail.com>
2011-10-08 02:05:24 +0200

    .

--- critbit_tree.c
+++ critbit_tree.c
@@ -5,23 +5,20 @@
 typedef struct {
 	void    *child[2];
 	int      byte;
-	uint8_t  bits;
+	uint8_t  bitmask;
 } critbit_node_t;
 
 typedef struct {
 	void *root;
 } critbit_tree_t;
 
-//
-//0 == posix_memalign(&memptr, sizeof(void *), size);
-//
+bool cbtree_contain_str(critbit_tree_t *tree, const char *str);
+bool cbtree_contain(critbit_tree_t *tree, const uint8_t *data, size_t len);
 
-/*
-bool cbtree_contain_str(critbit_tree_t *t, const char *s)
+bool cbtree_contain_str(critbit_tree_t *tree, const char *str)
 {
-	return cbtree_contain(t, s, strlen(s));
+	return cbtree_contain(tree, (uint8_t *) str, 0);
 }
-*/
 
 bool cbtree_contain(critbit_tree_t *tree, const uint8_t *data, size_t len)
 {
@@ -29,6 +26,8 @@ bool cbtree_contain(critbit_tree_t *tree, const uint8_t *data, size_t len)
 
 	if (!p)
 		return false;
+	if (!len)
+		len = strlen((char *) data);
 
 	while ((intptr_t) p & 1) {
 		critbit_node_t *q = p - 1;
@@ -37,20 +36,12 @@ bool cbtree_contain(critbit_tree_t *tree, const uint8_t *data, size_t len)
 
 		if (q->byte < len)
 			c = data[q->byte];
-
-		dir = ((q->bits | c) + 1) >> 8;
-
-
-
-
-
-
+		dir = ((q->bitmask | c) + 1) >> 8;
 		p = q->child[dir];
 	}
 
-
-
-
-
-	return !strcmp(s, p);
+	if (len)
+		return memcmp(data, p, len);
+	else
+		return strcmp((char *) data, (char *) p) == 0;
 }
